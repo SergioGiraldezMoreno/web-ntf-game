71c01c9a02751f098a267214bc00bd4d13337984
Sergio Giraldez Moreno <sergi.giraldez.moreno@gmail.com>
2021-09-09 18:09:42 +0200

    some styles in the home page info boxes and the base font size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,25 @@
         <div class="main-div row text-white h-100 d-flex align-items-start text-center">
             <div>
                 <h1 class="main-title shining-text">HOME</h1>
-                <div class="row d-none d-lg-inline" ><div style="height: 55vh"></div></div>
+                <div class="row d-none d-lg-inline" ><div style="height: 50vh"></div></div>
                 <div class="row equal row-cols-1 row-cols-lg-3 justify-content-center">
                     <div class="col my-4 my-lg-0">
                         <div class="p-4 info-box h-100 justify-content-center m-auto d-flex flex-column" style="width: 90%">
-                            <h3>Whitepaper</h3>
+                            <h2 class="yellow-text">Whitepaper</h2>
                             <p>All you need to know about the project. The technologies, game dynamics, goals...</p>
                             <button class="mt-auto">Read more</button>
                         </div>
                     </div>
                     <div class="col my-4 my-lg-0">
                         <div class="p-4 info-box h-100 justify-content-center m-auto d-flex flex-column" style="width: 90%">
-                            <h3>Roadmap</h3>
-                            <p>All the stages, deadlines and the current status of the project</p>
+                            <h2 class="yellow-text">Roadmap</h2>
+                            <p>All the information about stages, deadlines and the current status of the project</p>
                             <button class="mt-auto">Read more</button>
                         </div>
                     </div>
                     <div class="col my-4 my-lg-0">
                         <div class="p-4 info-box h-100 justify-content-center m-auto d-flex flex-column" style="width: 90%">
-                            <h3>Team</h3>
+                            <h2 class="yellow-text">Team</h2>
                             <p>Get to know all the team members and their background</p>
                             <button class="mt-auto">Read more</button>
                         </div>
